5d871e9c53e5cb797955d30583e7aea602114473
2kgkkg4444 <anotheronejustincaseyk@gmail.com>
2026-03-19 11:35:01 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Strrev Tools
+// @namespace    http://tampermonkey.net/
+// @version      9.0
+// @description  Multi-account manager, silent sniper, catalog buyer & trader for strrev.com
+// @author       You
+// @match        https://www.strrev.com/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_xmlhttpRequest
+// @connect      www.strrev.com
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/utils.js
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/accounts.js
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/buy.js
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/sniper.js
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/catalog.js
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/trade.js
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/ui.js
+// ==/UserScript==
+
+init();
