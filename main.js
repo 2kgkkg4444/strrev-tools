d97ddadddbd1bcbb6a084bcaabb9aab0965455be
2kgkkg4444 <anotheronejustincaseyk@gmail.com>
2026-03-19 20:24:38 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Strrev Tools
 // @namespace    http://tampermonkey.net/
-// @version      9.6
+// @version      14.1
 // @description  Multi-account manager, silent sniper, catalog buyer, trader, friend requester & daily chest for strrev.com
 // @author       You
 // @match        https://www.strrev.com/*
@@ -9,15 +9,15 @@
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @connect      www.strrev.com
-// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/utils.js?v=8
-// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/accounts.js?v=8
-// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/buy.js?v=8
-// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/sniper.js?v=8
-// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/catalog.js?v=8
-// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/trade.js?v=8
-// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/friends.js?v=8
-// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/daily.js?v=8
-// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/ui.js?v=8
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/utils.js?v=52
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/accounts.js?v=52
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/buy.js?v=52
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/sniper.js?v=52
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/catalog.js?v=52
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/trade.js?v=52
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/friends.js?v=52
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/daily.js?v=52
+// @require      https://raw.githubusercontent.com/2kgkkg4444/strrev-tools/main/ui.js?v=52
 // ==/UserScript==
 
 init();
